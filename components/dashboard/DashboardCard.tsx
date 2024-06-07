11639229a66dd721d1cad800216c0469f165fc39
connor mccarthy <74563791+Connok@users.noreply.github.com>
2024-06-07 18:53:51 -0400

    Main Dashboard Posts

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -1,13 +1,21 @@
 import {Card, CardContent} from "@/components/ui/card"
-import { Newspaper } from "lucide-react";
 
-const DashboardCard = () => {
+import { LucideIcon } from "lucide-react";
+
+
+interface DashboardCardProps {
+    title: string;
+    count: number;
+    icon: React.ReactElement<LucideIcon>;
+}
+
+const DashboardCard = ({title, count, icon}: DashboardCardProps) => {
     return <Card className="bg-slate-100 dark:bg-slate-800 p-4 pb-0">
         <CardContent>
-            <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">Posts</h3>
+            <h3 className="text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200">{ title }</h3>
             <div className="flex gap-5 justify-center items-center">
-                <Newspaper className='text-slate-500' size={72}/>
-                <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">200</h3>
+                { icon }
+                <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">{ count }</h3>
             </div>
         </CardContent>
     </Card>; 
